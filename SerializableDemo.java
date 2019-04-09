92895ed8adf3994ae9b6b72baa01ff35147c86e1
Mastan vali <mastanvali343@gmail.com>
2019-04-10 05:18:10 +0530

    added logic for serialization

--- SerializableDemo.java
+++ SerializableDemo.java
@@ -1,7 +1,36 @@
 package serializable;
+import java.io.*;
+import java.util.*;
 
-class SerializableDemo {
+class SerializableDemo implements Serializable{
+
+	String name;
+	int rollNo;
+
+	public SerializableDemo(String name, int rollNo) {
+		this.name = name;
+		this.rollNo = rollNo;
+	}
+
    public static void main(String... ar) {
+
+	SerializableDemo sd = new SerializableDemo("mastan",101);
+   try{
+	System.out.println("Serialization started....!");
+	
+	FileOutputStream fos = new FileOutputStream("mastan.txt");
+	ObjectOutputStream oos = new ObjectOutputStream(fos);
+	oos.writeObject(sd);
+	oos.close();
+	
+	System.out.println("Serialization ended....!");
+   }
+   catch(FileNotFoundException fnfe) {
+	fnfe.printStackTrace();
+   }
+   catch(IOException ioe) { 
+	ioe.printStackTrace();
+   }
 	
    }
 }
