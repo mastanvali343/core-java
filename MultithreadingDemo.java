16501b06024111b9ac6ad7d0bdb1e66495ded40e
Mastan vali <mastanvali343@gmail.com>
2019-04-10 12:08:59 +0530

    MultithreadingDemo.java file added

---
+++ MultithreadingDemo.java
@@ -0,0 +1,13 @@
+package multithreading;
+class MultithreadingDemo extends Thread {
+   public void run() {
+	System.out.println("run()");
+   }
+   public static void main(String... ar) {
+	
+	MultithreadingDemo md = new MultithreadingDemo();
+	md.start();
+  }
+}
+   	
+	
