78e80a73817ded93134425fa211d5dbd7f136732
Mastan vali <mastanvali343@gmail.com>
2019-04-10 05:58:53 +0530

    added logic for DeSerialization

--- SerializableDemo.java
+++ SerializableDemo.java
@@ -24,6 +24,18 @@ class SerializableDemo implements Serializable{
 	oos.close();
 	
 	System.out.println("Serialization ended....!");
+
+	System.out.println("DeSerialization started....!");
+	
+	FileInputStream fis = new FileInputStream("mastan.txt");
+	ObjectInputStream ois = new ObjectInputStream(fis);
+	Object obj = ois.readObject();
+	SerializableDemo sd1 = (SerializableDemo)obj;
+	System.out.println(sd1.name);
+	System.out.println(sd1.rollNo);
+	ois.close();
+	
+	System.out.println("DeSerialization ended....!");
    }
    catch(FileNotFoundException fnfe) {
 	fnfe.printStackTrace();
@@ -31,6 +43,9 @@ class SerializableDemo implements Serializable{
    catch(IOException ioe) { 
 	ioe.printStackTrace();
    }
+   catch(ClassNotFoundException cnfe) {
+	cnfe.printStackTrace();
+  }
 	
    }
 }
